56d9ddd77e2d488d8ced38e7f85a8dbdbb62efe5
fukukawa <fukulab11@icloud.com>
2024-03-09 17:50:57 +0900

    メール未認証でもアプリ利用できるように変更

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -19,7 +19,7 @@ class ProfileController extends Controller
     public function edit(Request $request): Response
     {
         return Inertia::render('Profile/Edit', [
-            'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
+            'mustVerifyEmail' => true ?? $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
         ]);
     }
