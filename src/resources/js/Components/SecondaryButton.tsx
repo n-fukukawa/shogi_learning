a55ea38189cafa71a6fe2be099ef4cc326053bef
fukukawa <fukulab11@icloud.com>
2024-03-10 13:54:06 +0900

    スタイルが当たらない場合があるため修正

--- src/resources/js/Components/SecondaryButton.tsx
+++ src/resources/js/Components/SecondaryButton.tsx
@@ -14,8 +14,8 @@ export default function SecondaryButton({
       type={type}
       className={
         `block py-2 bg-white border border-stone-300 rounded-md text-center font-semibold text-sm text-stone-600 uppercase tracking-widest shadow-sm hover:bg-gray-100 disabled:opacity-25 transition ease-in-out duration-150 ${
-          disabled && 'opacity-25'
-        } ${size === 'small' ? 'px-2' : 'px-4'} ` + className
+          disabled && 'opacity-25 '
+        } ${size === 'small' ? 'px-2 ' : 'px-4 '} ` + className
       }
       disabled={disabled}
     >
